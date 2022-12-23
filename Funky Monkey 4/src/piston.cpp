e12c227cc7a3377fcf2465672e20705c49b59bc1
Michael Cassidy <45998664+Michael-Lancebotics@users.noreply.github.com>
2022-12-23 11:36:09 -0500

    Update piston.cpp

--- Funky Monkey 4/src/piston.cpp
+++ Funky Monkey 4/src/piston.cpp
@@ -20,12 +20,6 @@ void Piston::switchState(){
     piston->set_value(reverse ? !newState : newState);
     printBrainText(2, "extend");
   }
-  // if(fourBar.claw.getState() == PistonState::retract){
-  //   printBrainText(4, "retract");
-  // }
-  // else if(fourBar.claw.getState() == PistonState::extend){
-  //   printBrainText(4, "extend");
-  // }
 }
 
 void Piston::extend(){
